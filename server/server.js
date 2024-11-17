85882bf713275b7dba56a3aa00c537f112c9cc99
ramez <ramezgamel52@gmail.com>
2024-11-17 15:51:25 +0200

    delete out vercel

--- server/server.js
+++ server/server.js
@@ -1,21 +1,26 @@
-require("dotenv").config();
+const dotenv = require("dotenv");
+const dbConnection = require("./utils/dbConnection");
 const app = require("./app");
 
-process.on("uncaughtException", (err) => {
-  console.log("UnCaught", err.name, err.message);
-  process.exit(1);
-});
+function server() {
+  dotenv();
 
-const dbConnection = require('./utils/dbConnection')
-dbConnection();
-const server = app.listen(process.env.PORT || 7000, () =>
-  console.log(`Server Running, port: ${process.env.PORT}`)
-);
-
-process.on("unhandledRejection", (err) => {
-  console.log(`Unhandled Rejection Errors: ${err}, Message:${err.message}`);
-  server.close(() => {
-    console.error("Shutting down...");
+  process.on("uncaughtException", (err) => {
+    console.log("UnCaught", err.name, err.message);
     process.exit(1);
   });
-});
+
+  dbConnection();
+  const server = app.listen(process.env.PORT || 7000, () =>
+    console.log(`Server Running, port: ${process.env.PORT}`)
+  );
+
+  process.on("unhandledRejection", (err) => {
+    console.log(`Unhandled Rejection Errors: ${err}, Message:${err.message}`);
+    server.close(() => {
+      console.error("Shutting down...");
+      process.exit(1);
+    });
+  });
+}
+server();
