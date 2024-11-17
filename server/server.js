2058247ee4c96ec6f5b7b7374e1ebb9c1c33dbdd
ramez <ramezgamel52@gmail.com>
2024-11-17 15:57:24 +0200

    deploy

--- server/server.js
+++ server/server.js
@@ -1,26 +1,21 @@
-const dotenv = require("dotenv");
-const dbConnection = require("./utils/dbConnection");
+require("dotenv").config();
 const app = require("./app");
+const dbConnection = require("./utils/dbConnection");
 
-function server() {
-  dotenv();
+process.on("uncaughtException", (err) => {
+  console.log("UnCaught", err.name, err.message);
+  process.exit(1);
+});
 
-  process.on("uncaughtException", (err) => {
-    console.log("UnCaught", err.name, err.message);
+dbConnection();
+const server = app.listen(process.env.PORT || 7000, () =>
+  console.log(`Server Running, port: ${process.env.PORT}`)
+);
+
+process.on("unhandledRejection", (err) => {
+  console.log(`Unhandled Rejection Errors: ${err}, Message:${err.message}`);
+  server.close(() => {
+    console.error("Shutting down...");
     process.exit(1);
   });
-
-  dbConnection();
-  const server = app.listen(process.env.PORT || 7000, () =>
-    console.log(`Server Running, port: ${process.env.PORT}`)
-  );
-
-  process.on("unhandledRejection", (err) => {
-    console.log(`Unhandled Rejection Errors: ${err}, Message:${err.message}`);
-    server.close(() => {
-      console.error("Shutting down...");
-      process.exit(1);
-    });
-  });
-}
-server();
+});
